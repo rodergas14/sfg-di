6b85fdbff31703a6ca6e14c8cfb4ef323b196d7a
rodergas14 <robertalmar@gmail.com>
2020-11-11 22:04:18 +0100

    Types of injection with plain Java

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService{
+
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return "Hello World";
+	}
+	
+	
+}
